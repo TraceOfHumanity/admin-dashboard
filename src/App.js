270b723b07367964bf7f68f20d512abdeb570dc2
Alexander_Ovchar <azalexxx02@gmail.com>
2023-12-05 03:52:57 +0200

    ready dashboard for PC

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import { Route, Routes } from "react-router-dom";
 
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
+import Bar from "./scenes/bar";
+import CalendarC from "./scenes/calendar/calendar";
 import Contacts from "./scenes/contacts";
 import Dashboard from "./scenes/dashboard";
+import FAQ from "./scenes/faq";
 import Form from "./scenes/form";
+import Geography from "./scenes/geography";
 import SidebarComponent from "./scenes/global/Sidebar";
 import Topbar from "./scenes/global/Topbar";
 import Invoices from "./scenes/invoices";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
 import Team from "./scenes/team/Index";
 import { ColorModeContext, useMode } from "./theme";
 
@@ -27,12 +33,12 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
-              {/* <Route path="/bar" element={<Bar />} /> */}
-              {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
-              {/* <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<CalendarC />} />
+              <Route path="/geography" element={<Geography />} />
             </Routes>
           </main>
         </div>
